3ecce5013ff5773033c2779266831bc4056cc8dd
Anshuman Negi <negsanshuman14@gmail.com>
2025-04-05 14:35:25 +0530

    Allow external image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['thumbs.dreamstime.com','c8.alamy.com','tse3.mm.bing.net','www.labtestsguide.com','i.ytimg.com','bookmerilab.com','lifecarediagnostic.com','i.ytimg.com','d1vbn70lmn1nqe.cloudfront.net',''],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
